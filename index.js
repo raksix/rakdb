e692ff4c23419a3c6bfc342fe1fa00fb7039d0ca
Raksix <raksixoffical@gmail.com>
2021-04-01 17:27:51 +0300

    H

--- index.js
+++ index.js
@@ -29,30 +29,21 @@ rakdb.yenidb = (payload) => {
 
 
 var addToObject = function (obj, key, value, index) {
-
     var temp = {};
     var i = 0;
-
     for (var prop in obj) {
         if (obj.hasOwnProperty(prop)) {
-
             if (i === index && key && value) {
                 temp[key] = value;
             }
-
             temp[prop] = obj[prop];
-
             i++;
-
         }
     }
-
     if (!index && key && value) {
         temp[key] = value;
     }
-
     return temp;
-
 };
 
 
